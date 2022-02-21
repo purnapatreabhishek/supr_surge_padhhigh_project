6b8d72ffa080ead773032a972d42835a5a9beca8
purnapatreabhishek <80238842+purnapatreabhishek@users.noreply.github.com>
2022-02-21 23:07:29 +0530

    update 17

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,12 +14,12 @@
 	function loader() {
 		$(window).on('load', function () {
 			$('#ctn-preloader').addClass('loaded');
-			$("#loading").fadeOut(500);
+			$("#loading").fadeOut(200);
 			// Una vez haya terminado el preloader aparezca el scroll
 
 			if ($('#ctn-preloader').hasClass('loaded')) {
 				// Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
-				$('#preloader').delay(900).queue(function () {
+				$('#preloader').delay(400).queue(function () {
 					$(this).remove();
 				});
 			}
